29e348df4b76208749df263849fc24685ab2cac2
Angela Flowers <me@angela.flowers>
2020-02-28 12:10:48 -0500

    implemented binary search tree for names.py

--- names/names.py
+++ names/names.py
@@ -13,10 +13,53 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+class NameSearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left:
+                self.left.insert(value)
+            else:
+                self.left = NameSearchTree(value)                
+        else:
+            if self.right:
+                self.right.insert(value)    
+            else:
+                self.right = NameSearchTree(value)
+                
+                    
+    def contains(self, value):
+        if value == self.value:
+            return True
+        elif value < self.value and self.left:
+            return self.left.contains(value)
+        elif value > self.value and self.right:
+            return self.right.contains(value)
+        else:
+            return False
+
+
+name_tree = NameSearchTree("names to search")
+
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    name_tree.insert(name_1)
+
+for name_2 in names_2:
+    if name_tree.contains(name_2):
+        duplicates.append(name_2)
+
+        
+
+
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
